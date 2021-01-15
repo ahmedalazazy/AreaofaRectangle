7c4e7667bdc6a80a5f81af1366c7b5295bf3d49c
ahmedalazazy <51747858+ahmedalazazy@users.noreply.github.com>
2021-01-15 23:00:58 +0200

    Add files via upload

---
+++ rectanglearea.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//The input from The user
+    int width,length;
+//  int area
+    cout << "Please enter rectangle Width \n";
+    cin >>width;
+    cout << "Please enter rectangle Length\n";
+    cin >> length;
+//processing
+  //  area = width * length;
+//output
+    cout << "The Rectanglr Area equal : " << width * length<< endl;
+
+}
